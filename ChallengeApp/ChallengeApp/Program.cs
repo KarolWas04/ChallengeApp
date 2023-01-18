81496b2177b7c4adba68d29b1417e531a9da95ba
karol <karol.wasilewski04@gmail.com>
2023-01-18 17:56:43 +0100

    Wyzwanie21 dzien4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+char sex = 'K';
+int age = 30;
+
+if (sex == 'K')
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta ponizej 30");
+    }
+    else if (age > 30)
+    {
+        Console.WriteLine("Kobieta powyzej 30");
+    }
+    else if (name == "Ewa" && age == 30)
+    {
+        Console.WriteLine("Ewa lat 30");
+    }
+}
+else if (sex != 'K')
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepelnoletni mezczyzna");
+    }
+    else  if (age > 18)
+    {
+        Console.WriteLine("Pelnoletni mezczyzna");
+    }
+}
