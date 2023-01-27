99b2eaacf864f5e090435f7e8a26db97ff8f7e66
karol <karol.wasilewski04@gmail.com>
2023-01-27 18:48:15 +0100

    wyzwanie21dzien7

---
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTest1.cs
@@ -0,0 +1,22 @@
+namespace ChallengeApp.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void WhenUserCollectThreeScores_ShouldCorrectResult()
+        {
+            // arrange
+            var user = new Employee("Karol", "Wasilewski", "33");
+            user.AddScore(5);
+            user.AddScore(6);
+            user.AddScore(-12);
+
+            // act
+            var result = user.Result;
+
+            // aasert
+            Assert.AreEqual(-1, result);
+            
+        }
+    }
+}
