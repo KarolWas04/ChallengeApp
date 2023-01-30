3cd42817304783be6fd647d6018147adb4971451
karol <karol.wasilewski04@gmail.com>
2023-01-30 15:19:43 +0100

    wyzwanie21dzien9

---
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace ChallengeApp.Tests
+{
+    public class EmployeeTests
+    {
+        [Test]
+        public void MinMaxAverage_ShouldBeCorrect()
+        {
+            //arrange
+
+            var employee = new Employee("Karol", "Wasilewski");
+            employee.AddGrade(5);
+            employee.AddGrade(7);
+            employee.AddGrade(15);
+
+            // act
+            var statistics = employee.GetStatistics();
+
+            // assert
+            Assert.AreEqual(5, statistics.Min);
+            Assert.AreEqual(15, statistics.Max);
+            Assert.AreEqual(9, statistics.Average);
+        }
+            
+
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,29 +1,11 @@
 ﻿using ChallengeApp;
-using System.Reflection.Metadata;
 
-Employee person1 = new Employee("Karol", "Wasilewski", "33");
-Employee person2 = new Employee("Janusz", "Kowalski", "54");
-Employee person3 = new Employee("Grazyna", "Kowalska", "50");
+var employee = new Employee("Karol", "Wasilewski");
+employee.AddGrade(2);
+employee.AddGrade(2);
+employee.AddGrade(6);
 
-person1.AddScore(5); person1.AddScore(2); person1.AddScore(3); person1.AddScore(9); person1.AddScore(4);
-
-person2.AddScore(2); person2.AddScore(4); person2.AddScore(9); person2.AddScore(1); person2.AddScore(8);
-
-person3.AddScore(6); person3.AddScore(5); person3.AddScore(8); person3.AddScore(4); person3.AddScore(3);
-
-
-List<Employee> people = new List<Employee>()
-{
-    person1, person2, person3
-};
-
-int maxResult = -1;
-Employee personWithMaxResult = null;
-foreach(var person in people)
-{
-    if(person.Result > maxResult)
-    {
-        personWithMaxResult = person;
-    }
-}
-Console.WriteLine($"Osoba z najwyzsza liczba ocen to: {personWithMaxResult.Name} {personWithMaxResult.Surname} w wieku {personWithMaxResult.Age} lat z wynikiem {personWithMaxResult.Result} punktow.");
+var statistics = employee.GetStatistics();
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Min: {statistics.Min}");
+Console.WriteLine($"Max: {statistics.Max}");
